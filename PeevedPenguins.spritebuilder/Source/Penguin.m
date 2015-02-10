c0851ec731afcc40a0b83054a90394cf65672fcb
CCCYUN <caoyun9006@gmail.com>
2015-02-09 20:23:46 -0800

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Source/Penguin.m
@@ -0,0 +1,23 @@
+//
+//  Penguin.m
+//  PeevedPenguins
+//
+//  Created by Yun on 2/9/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+#import "Penguin.h"
+
+@implementation Penguin
+
+- (id)init {
+    self = [super init];
+    
+    if (self) {
+        CCLOG(@"Penguin created");
+    }
+    
+    return self;
+}
+
+@end
